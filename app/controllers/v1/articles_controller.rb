9df4174e8b45cc73a5ccbbae406d973875753217
Moneshsoni <sonimonesh115@gmail.com>
2023-09-29 16:47:59 +0530

    add the comments api

---
+++ app/controllers/v1/articles_controller.rb
@@ -0,0 +1,4 @@
+class V1::ArticlesController < ApplicationController
+  def index
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_28_180800) do
+ActiveRecord::Schema.define(version: 2023_09_29_100956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "descriptions"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "book_name"
@@ -21,6 +28,15 @@ ActiveRecord::Schema.define(version: 2023_09_28_180800) do
     t.string "author_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "contacts", force: :cascade do |t|
@@ -46,4 +62,5 @@ ActiveRecord::Schema.define(version: 2023_09_28_180800) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "articles"
 end
